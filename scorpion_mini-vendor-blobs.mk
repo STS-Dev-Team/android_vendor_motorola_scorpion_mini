37ca90c6be43d4d6d1c646fc56b202c39884e553
dhacker29 <davidhackerdvm@gmail.com>
2012-10-27 01:24:40 -0500

    add wlan frimware from driver release

--- scorpion_mini-vendor-blobs.mk
+++ scorpion_mini-vendor-blobs.mk
@@ -54,6 +54,7 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/inis/WCNSS_qcom_cfg.ini.es1:system/etc/firmware/wlan/prima/inis/WCNSS_qcom_cfg.ini.es1 \
     $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_calibration.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_calibration.bin \
     $(VENDOR_BLOB_FOLDER)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_regulatory.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv_regulatory.bin \
     $(VENDOR_BLOB_FOLDER)/ics_adreno/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
